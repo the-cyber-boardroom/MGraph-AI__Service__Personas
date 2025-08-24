42c1225f9566d17c30fa6692493c74b0048b4d2b
Dinis Cruz <dinis.cruz@owasp.org>
2025-08-24 09:54:30 +0100

    fixed imports

--- tests/deploy_aws/test_Deploy__Service__base.py
+++ tests/deploy_aws/test_Deploy__Service__base.py
@@ -1,9 +1,9 @@
 import pytest
-from osbot_utils.utils.Misc                                        import list_set
-from osbot_fast_api_serverless.deploy.Deploy__Serverless__Fast_API import DEFAULT__ERROR_MESSAGE__WHEN_FAST_API_IS_OK
-from mgraph_ai_service_personas.config                                 import LAMBDA_DEPENDENCIES__FAST_API_SERVERLESS
-from mgraph_ai_service_personas.utils.Version                          import version__mgraph_ai_service_personas
-from mgraph_ai_service_personas.utils.deploy.Deploy__Service           import Deploy__Service
+from osbot_utils.utils.Misc                                             import list_set
+from osbot_fast_api_serverless.deploy.Deploy__Serverless__Fast_API      import DEFAULT__ERROR_MESSAGE__WHEN_FAST_API_IS_OK
+from mgraph_ai_service_personas.config                                  import LAMBDA_DEPENDENCIES__FAST_API_SERVERLESS
+from mgraph_ai_service_personas.utils.Version                           import version__mgraph_ai_service_personas
+from mgraph_ai_service_personas.utils.deploy.Deploy__Service            import Deploy__Service
 
 
 class test_Deploy__Service__base():     # Base class for deployment tests - override stage in subclasses
